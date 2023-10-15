a83634ade2ed67e3ddf4db679a235f061e8b5b96
Marco Rodriguez <48032991+Rodrig79@users.noreply.github.com>
2023-10-15 02:05:39 -0700

    added trophy icons to scoreboard

--- src/components/scoreboard-view/ScoreboardView.tsx
+++ src/components/scoreboard-view/ScoreboardView.tsx
@@ -16,30 +16,50 @@ const ScoreboardView: React.FC<Props> = ({}) => {
 
   const dispatch = useAppDispatch();
 
+  const resetScoreboard = () => {
+    const newScoreboard: ScoreboardType = {};
+    dispatch(setScoreboard(newScoreboard));
+  };
+
   // Convert scoreboard object to an array and sort by score
   const sortedKeys = Object.keys(scoreboard).sort(
     (a, b) => scoreboard[b].score - scoreboard[a].score
   );
 
-  const resetScoreboard = () => {
-    const newScoreboard: ScoreboardType = {};
-
-    dispatch(setScoreboard(newScoreboard));
-  };
+  // Calculate the number of players for each rank
+  const cardArrayLength = sortedKeys.length;
+  const diamondCutoff = Math.ceil(cardArrayLength * 0.05);
+  const goldCutoff = Math.ceil(cardArrayLength * 0.15);
+  const silverCutoff = Math.ceil(cardArrayLength * 0.5);
 
   return (
     <div className={"scoreboard_view"}>
-      {sortedKeys.map((key) => (
-        <div key={key} className="scoreboard_div">
-          <label className="scoreboard_title">{lensArray[key].title}</label>
-          <label className="scoreboard_score">{scoreboard[key].score}</label>
-        </div>
-      ))}
-      <button
-        onClick={resetScoreboard}
-      >
-        Reset Scoreboard
-      </button>
+      {sortedKeys.map((key, index) => {
+        type RankType = "bronze" | "silver" | "gold" | "diamond";
+        let rank: RankType = "bronze";
+
+        // Assign ranks based on index
+        if (index < diamondCutoff) {
+          rank = "diamond";
+        } else if (index < goldCutoff) {
+          rank = "gold";
+        } else if (index < silverCutoff) {
+          rank = "silver";
+        }
+
+        return (
+          <div key={key} className="scoreboard_div">
+              <label className={`scoreboard_score_trophy_${rank}`}>
+              🏆
+            </label>
+            <label className={`scoreboard_score_${rank}`}>
+              {scoreboard[key].score}
+            </label>
+            <label className="scoreboard_title">{lensArray[key].title}</label>
+          </div>
+        );
+      })}
+      <button onClick={resetScoreboard}>Reset Scoreboard</button>
     </div>
   );
 };
