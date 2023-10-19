ce2b9726c0d7535373e3c86ba58dbf05b18d69a2
Marco Rodriguez <48032991+Rodrig79@users.noreply.github.com>
2023-10-18 20:38:32 -0700

    styling updates

--- src/components/single-view/SingleCardView.tsx
+++ src/components/single-view/SingleCardView.tsx
@@ -14,31 +14,31 @@ import { selectCardIndex } from "../../redux-slices/user-data/UserDataSlice";
 interface Props {}
 
 const SingleCardView: React.FC<Props> = ({}) => {
-  const cardIndex = useAppSelector(selectCardIndex)
+  const cardIndex = useAppSelector(selectCardIndex);
   const [cardInfo, setCardInfo] = useState<GameLensType>(CardArray[0]);
 
   useEffect(() => {
-    if(!cardIndex){
-      return
+    if (!cardIndex) {
+      return;
     }
     const newCardInfo = CardArray?.[cardIndex];
     setCardInfo(newCardInfo);
-
   }, [cardIndex]);
 
   return (
     <>
-    {
-      cardInfo &&
-      <div className={"single_card"}>
-      <CardTitle cardInfo={cardInfo}  />
-      <CardImage cardInfo={cardInfo}/>
-      <CardClipboard cardInfo={cardInfo} />
-      <CardDescription cardInfo={cardInfo} />
-      <CardQuestions cardInfo={cardInfo} />
-      <PageButtons />
-    </div>
-    }
+      {cardInfo && (
+        <div className={"single_card_container"}>
+          <div className={"single_card"}>
+            <CardTitle cardInfo={cardInfo} />
+            <CardImage cardInfo={cardInfo} />
+            <CardClipboard cardInfo={cardInfo} />
+            <CardDescription cardInfo={cardInfo} />
+            <CardQuestions cardInfo={cardInfo} />
+            <PageButtons />
+          </div>
+        </div>
+      )}
     </>
   );
 };
