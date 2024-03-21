a189af1ded23124fd821c63bf479e72f1a05bc6e
Marco Rodriguez <48032991+Rodrig79@users.noreply.github.com>
2024-03-20 22:24:51 -0700

    amplify init

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,13 @@ import AppHeader from "./components/app-header/AppHeader";
 import AppBody from "./components/app-body/AppBody";
 import { useState } from "react";
 import SignInPage from "./components/sign-in/SignInPage";
+import { Amplify } from "aws-amplify";
+import awsconfig from "./aws-exports"; // path to your aws-exports file
 
 export default function App() {
   const [signedIn, setSignedIn] = useState(false);
+  // Configure Amplify with your aws-exports file
+  Amplify.configure(awsconfig);
 
   return (
     <Provider store={store}>
--- src/components/sign-in/SignInPage.tsx
+++ src/components/sign-in/SignInPage.tsx
@@ -1,14 +1,55 @@
 import {Amplify} from 'aws-amplify';
-import awsconfig from '../../aws-exports'; // path to your aws-exports file
+import { signInWithRedirect, signOut, getCurrentUser, AuthUser } from "aws-amplify/auth";
+import { Hub } from "aws-amplify/utils";
+import { useEffect, useState } from 'react';
 
-// Configure Amplify with your aws-exports file
-Amplify.configure(awsconfig);
 
 const SignInPage = () => {
+    const [user, setUser] = useState<AuthUser | null>(null);
+    const [error, setError] = useState<unknown>(null);
+    const [customState, setCustomState] = useState<string | null>("Custom State: None");
+  
+
+    useEffect(() => {
+        const unsubscribe = Hub.listen("auth", ({ payload }) => {
+          switch (payload.event) {
+            case "signInWithRedirect":
+              getUser();
+              break;
+            case "signInWithRedirect_failure":
+              setError("An error has occurred during the OAuth flow.");
+              break;
+            case "customOAuthState":
+              setCustomState(payload.data); // this is the customState provided on signInWithRedirect function
+              break;
+          }
+        });
+    
+        getUser();
+    
+        return unsubscribe;
+      }, []);
+    
+      const getUser = async (): Promise<void> => {
+        try {
+          const currentUser = await getCurrentUser();
+          setUser(currentUser);
+        } catch (error) {
+          console.error(error);
+          console.log("Not signed in");
+        }
+      };
+
 
 return(
     <div>
-
+      <button onClick={() => signInWithRedirect({ customState: "Custome State: shopping-cart"})}>Open Hosted UI</button>
+      <button onClick={() => signInWithRedirect({ provider: "Google", customState: "shopping-cart" })}>
+        Open Google
+      </button>
+      <button onClick={() => signOut()}>Sign Out</button>
+      <div>{user?.username}</div>
+      <div>{customState}</div>
     </div>
   );
 };
